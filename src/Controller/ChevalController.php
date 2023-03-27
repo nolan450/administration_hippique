a3309eb508cd5f7d96639ebf7249c9c7e423c559
Nolan ELASSI <nolan.el-assi@etu.univ-amu.fr>
2023-03-27 13:42:29 +0200

    quelques modifs

--- src/Controller/ChevalController.php
+++ src/Controller/ChevalController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Cheval;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ChevalController extends AbstractController
 {
@@ -14,6 +17,16 @@ class ChevalController extends AbstractController
     #[Route('/cheval', name: 'cheval')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        //création d'un cheval
+        $cheval = new Cheval();
+        $cheval->setNom("Cheval 1");
+        $cheval->setRace("Selle Français");
+        $cheval->setDescription("Cheval de sport");
+
+        // je persiste le cheval en base de données
+        $entityManager->persist($cheval);
+        $entityManager->flush();
+
         // je récupère la liste des chevaux en base de données
         $chevaux = $entityManager->getRepository(Cheval::class)->findAll();
 
--- src/Entity/Cheval.php
+++ src/Entity/Cheval.php
@@ -23,7 +23,7 @@ class Cheval
     private ?string $description = null;
 
     #[ORM\OneToOne(inversedBy: 'cheval', cascade: ['persist', 'remove'])]
-    private ?joueur $id_propietaire = null;
+    private ?Joueur $id_propietaire = null;
 
     #[ORM\OneToOne(mappedBy: 'id_cheval', cascade: ['persist', 'remove'])]
     private ?Etats $etats = null;
@@ -75,12 +75,12 @@ class Cheval
         return $this;
     }
 
-    public function getIdPropietaire(): ?joueur
+    public function getIdPropietaire(): ?Joueur
     {
         return $this->id_propietaire;
     }
 
-    public function setIdPropietaire(?joueur $id_propietaire): self
+    public function setIdPropietaire(?Joueur $id_propietaire): self
     {
         $this->id_propietaire = $id_propietaire;
 
